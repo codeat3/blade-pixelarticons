45fd689a0394a34179ab6637d31a0f3e8c9d801f
swapnilsarwe <swapnilsarwe@users.noreply.github.com>
2023-02-18 14:32:54 +0000

    Fix styling

--- src/BladePixelArtIconsServiceProvider.php
+++ src/BladePixelArtIconsServiceProvider.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace Codeat3\BladePixelArtIcons;
 
 use BladeUI\Icons\Factory;
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Contracts\Container\Container;
+use Illuminate\Support\ServiceProvider;
 
 final class BladePixelArtIconsServiceProvider extends ServiceProvider
 {
@@ -17,24 +17,24 @@ final class BladePixelArtIconsServiceProvider extends ServiceProvider
         $this->callAfterResolving(Factory::class, function (Factory $factory, Container $container) {
             $config = $container->make('config')->get('blade-pixelarticons', []);
 
-            $factory->add('pixelarticons', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
+            $factory->add('pixelarticons', array_merge(['path' => __DIR__ . '/../resources/svg'], $config));
         });
     }
 
     private function registerConfig(): void
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/blade-pixelarticons.php', 'blade-pixelarticons');
+        $this->mergeConfigFrom(__DIR__ . '/../config/blade-pixelarticons.php', 'blade-pixelarticons');
     }
 
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../resources/svg' => public_path('vendor/blade-pixelarticons'),
+                __DIR__ . '/../resources/svg' => public_path('vendor/blade-pixelarticons'),
             ], 'blade-pixelarticons');
 
             $this->publishes([
-                __DIR__.'/../config/blade-pixelarticons.php' => $this->app->configPath('blade-pixelarticons.php'),
+                __DIR__ . '/../config/blade-pixelarticons.php' => $this->app->configPath('blade-pixelarticons.php'),
             ], 'blade-pixelarticons-config');
         }
     }
